6e72a3f9e96905e97a059398d5b2cb12ebb44665
Josh Fletcher <slwd89@gmail.com>
2019-12-01 19:47:01 -0800

    Formatting only.

--- fevs-practicemanager-quickbooks-core/src/test/java/com/fevs/quickbooks/BasicTest.java
+++ fevs-practicemanager-quickbooks-core/src/test/java/com/fevs/quickbooks/BasicTest.java
@@ -12,27 +12,27 @@ import static org.junit.Assert.*;
 
 public class BasicTest {
 
-    OAuth2Config oauth2Config;
+  OAuth2Config oauth2Config;
 
-    private String clientId;
-    private String clientSecret;
+  private String clientId;
+  private String clientSecret;
 
-    @Before
-    public void setUp() {
+  @Before
+  public void setUp() {
 
-        clientId = Framework.getProperty(QuickBooksConstants.PROPERTY_CLIENT_ID);
-        clientSecret = Framework.getProperty(QuickBooksConstants.PROPERTY_CLIENT_SECRET);
+    clientId = Framework.getProperty(QuickBooksConstants.PROPERTY_CLIENT_ID);
+    clientSecret = Framework.getProperty(QuickBooksConstants.PROPERTY_CLIENT_SECRET);
 
-        oauth2Config = new OAuth2Config.OAuth2ConfigBuilder(clientId, clientSecret) //set client id, secret
-                .callDiscoveryAPI(Environment.SANDBOX) // call discovery API to populate urls
-                .buildConfig();
-    }
+    oauth2Config = new OAuth2Config.OAuth2ConfigBuilder(clientId, clientSecret) //set client id, secret
+            .callDiscoveryAPI(Environment.SANDBOX) // call discovery API to populate urls
+            .buildConfig();
+  }
 
-    @Test
-    public void testConstructorWithDomain() {
-        Request request = new Request.RequestBuilder(MethodType.GET, oauth2Config.getIntuitBearerTokenEndpoint()).build();
-        assertNotNull(request);
-        assertEquals(MethodType.GET, request.getMethod());
-        assertEquals(oauth2Config.getIntuitBearerTokenEndpoint(), request.getUrl());
-    }
+  @Test
+  public void testConstructorWithDomain() {
+    Request request = new Request.RequestBuilder(MethodType.GET, oauth2Config.getIntuitBearerTokenEndpoint()).build();
+    assertNotNull(request);
+    assertEquals(MethodType.GET, request.getMethod());
+    assertEquals(oauth2Config.getIntuitBearerTokenEndpoint(), request.getUrl());
+  }
 }
